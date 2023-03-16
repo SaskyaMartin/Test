52360c1ae1525111a094441aa9e98b6bf3a94e03
Seraphim <128052345+SaskyaMartin@users.noreply.github.com>
2023-03-16 14:41:27 +0100

    add files

---
+++ IfundElse.py
@@ -0,0 +1,6 @@
+Passwort = "Katze2"         #Passwort setzen
+PasswortEingabe = input("Geben Sie das Passwort ein:")      #Eingabe des Passworts
+if Passwort == PasswortEingabe:         #überprüfen ob die beiden Stings gleich sind
+    print("Sie haben das richtige Passwort eingegeben")
+else:                                   #Wenn die Strings nicht gleich sind
+    print("Sie haben das falsche Passwort eingegeben")
